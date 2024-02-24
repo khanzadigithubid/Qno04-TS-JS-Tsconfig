e25bce24428b75566882608a833b37a38a46a65b
Khanzadi Wazir Ali <160390882+khanzadigithubid@users.noreply.github.com>
2024-02-24 13:24:54 +0500

    Qno04 Ts/Js/Tsconfig and node js of 45 Assignments
    Qno04 Ts/Js/Tsconfig and node js of 45 Assignments

---
+++ Qno4.ts
@@ -0,0 +1,8 @@
+/*Q4. Famous Quote: Find a quote from a famous person you admire. Print the quote and the
+ name of its author. Your output should look something like the following, including the
+ quotation marks:
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+*/
+let quote = "Quaid-e-Azam once said";
+console.log(`${quote}, “With faith, discipline and selfless devotion to duty, there is 
+nothing worthwhile that you cannot achieve.”`);
